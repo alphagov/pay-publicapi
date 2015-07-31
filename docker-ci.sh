404c27d2e1a6a5239f68b7b4ad7c22960872becf
Mark Harris <mark.harris@digital.cabinet-office.gov.uk>
2015-07-31 16:25:25 +0100

    Removed unnecessary sanity check.

--- docker-ci.sh
+++ docker-ci.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
 COMMIT=$(git rev-parse HEAD)
-
-if [[ `git status --porcelain` ]]; then
-    >&2 echo "Not building - local changes found :("
-    exit 1
-fi
-
 IMAGE="govukpay/broker:$COMMIT"
 mvn clean package && docker build -t $IMAGE . && docker push $IMAGE
