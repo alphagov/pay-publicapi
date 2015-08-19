7e0feb022c994420ff68913f3544b5b49af81ca8
Ian Maddison <ian@i.need.stronger.coffee>
2015-08-19 14:27:29 +0100

    Fix commit tag for pr as per pay-connector

--- docker-ci.sh
+++ docker-ci.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-COMMIT=$(git rev-parse HEAD)
+#!/bin/bash
+if [ "x${ghprbActualCommit}" = "x" ]; then
+    COMMIT=$(git rev-parse HEAD)
+else
+    COMMIT=$ghprbActualCommit
+fi
+
 IMAGE="govukpay/publicapi:$COMMIT"
 mvn clean package && docker build -t $IMAGE . && docker push $IMAGE
