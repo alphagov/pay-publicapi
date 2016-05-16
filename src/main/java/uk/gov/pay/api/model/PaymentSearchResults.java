44af1b7e9ad4f329a4bed492567eb87f31294456
rahul-vinayak <rahul_vinayak@hotmail.com>
2016-05-16 23:31:51 +0100

    PP-335 HAL pagination/navigation links in public api payments search
    - gets the navigation links from the connector and builds public api
    links for payments search
    - accepts page and display_size request params for payments search
    - test fixes

--- src/main/java/uk/gov/pay/api/model/PaymentSearchResults.java
+++ src/main/java/uk/gov/pay/api/model/PaymentSearchResults.java
@@ -10,15 +10,6 @@ public class PaymentSearchResults {
     @JsonProperty(value = "results")
     private List<PaymentForSearchResult> payments;
 
-    @JsonProperty(value= "total")
-    private int total;
-
-    @JsonProperty(value="count")
-    private int count;
-
-    @JsonProperty(value="_links")
-    private List<Object> links;
-
     public PaymentSearchResults(List<PaymentForSearchResult> payments) {
         this.payments = payments;
     }
@@ -27,23 +18,4 @@ public class PaymentSearchResults {
         return payments;
     }
 
-    public void setPayments(List<PaymentForSearchResult> payments) {
-        this.payments = payments;
-    }
-
-    public void setTotal(int total) {
-        this.total = total;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
-    }
-
-    public List<Object> getLinks() {
-        return links;
-    }
-
-    public void setLinks(List<Object> links) {
-        this.links = links;
-    }
 }
