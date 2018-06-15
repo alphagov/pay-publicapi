65b8ccf6553089e65134cdf60219314a7ec69fa5
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2018-06-15 11:40:21 +0100

    PP-3679 Refactor to use PaymentSearchService in PaymentsResource
    - move search logic from resource to service

--- src/main/java/uk/gov/pay/api/resources/PaymentsResource.java
+++ src/main/java/uk/gov/pay/api/resources/PaymentsResource.java
@@ -2,35 +2,29 @@ package uk.gov.pay.api.resources;
 
 import black.door.hate.HalRepresentation;
 import com.codahale.metrics.annotation.Timed;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.auth.Auth;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import org.apache.commons.lang3.tuple.Pair;
 import org.apache.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import uk.gov.pay.api.app.config.PublicApiConfig;
 import uk.gov.pay.api.auth.Account;
 import uk.gov.pay.api.exception.CancelChargeException;
 import uk.gov.pay.api.exception.GetChargeException;
 import uk.gov.pay.api.exception.GetEventsException;
-import uk.gov.pay.api.exception.SearchChargesException;
 import uk.gov.pay.api.model.ChargeFromResponse;
 import uk.gov.pay.api.model.CreatePaymentRequest;
 import uk.gov.pay.api.model.PaymentError;
 import uk.gov.pay.api.model.PaymentEvents;
-import uk.gov.pay.api.model.PaymentForSearchResult;
-import uk.gov.pay.api.model.PaymentSearchResponse;
 import uk.gov.pay.api.model.PaymentSearchResults;
 import uk.gov.pay.api.model.links.PaymentWithAllLinks;
 import uk.gov.pay.api.service.ConnectorUriGenerator;
 import uk.gov.pay.api.service.CreatePaymentService;
+import uk.gov.pay.api.service.PaymentSearchService;
 import uk.gov.pay.api.service.PublicApiUriGenerator;
 
 import javax.inject.Inject;
@@ -39,28 +33,18 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.ProcessingException;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
-import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.lang.String.format;
-import static java.util.Arrays.asList;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import static org.apache.http.HttpStatus.SC_OK;
-import static uk.gov.pay.api.validation.PaymentSearchValidator.validateSearchParameters;
 
 @Path("/")
 @Api(value = "/", description = "Public Api Endpoints")
@@ -68,25 +52,24 @@ import static uk.gov.pay.api.validation.PaymentSearchValidator.validateSearchPar
 public class PaymentsResource {
 
     private static final Logger logger = LoggerFactory.getLogger(PaymentsResource.class);
-
-    private final String baseUrl;
-
+    
     private final Client client;
     private final CreatePaymentService createPaymentService;
-    private final ObjectMapper objectMapper;
     private final PublicApiUriGenerator publicApiUriGenerator;
     private final ConnectorUriGenerator connectorUriGenerator;
+    private final PaymentSearchService paymentSearchService;
 
     @Inject
-    public PaymentsResource(Client client, CreatePaymentService createPaymentService,
-                            ObjectMapper objectMapper, PublicApiConfig configuration,
-                            PublicApiUriGenerator publicApiUriGenerator, ConnectorUriGenerator connectorUriGenerator) {
+    public PaymentsResource(Client client, 
+                            CreatePaymentService createPaymentService, 
+                            PaymentSearchService paymentSearchService,
+                            PublicApiUriGenerator publicApiUriGenerator, 
+                            ConnectorUriGenerator connectorUriGenerator) {
         this.client = client;
         this.createPaymentService = createPaymentService;
-        this.baseUrl = configuration.getBaseUrl();
-        this.objectMapper = objectMapper;
         this.publicApiUriGenerator = publicApiUriGenerator;
         this.connectorUriGenerator = connectorUriGenerator;
+        this.paymentSearchService = paymentSearchService;
     }
 
     @GET
@@ -211,91 +194,28 @@ public class PaymentsResource {
                                    @QueryParam("page") String pageNumber,
                                    @ApiParam(value = "Number of results to be shown per page, should be a positive integer (optional, defaults to 500, max 500)", hidden = false)
                                    @QueryParam("display_size") String displaySize,
+                                   @ApiParam(value = "Direct Debit Agreement Id", hidden = false)
+                                   @QueryParam("agreement") String agreement,
                                    @Context UriInfo uriInfo) {
 
         logger.info("Payments search request - [ {} ]",
                 format("reference:%s, email: %s, status: %s, card_brand %s, fromDate: %s, toDate: %s, page: %s, display_size: %s",
                         reference, email, state, cardBrand, fromDate, toDate, pageNumber, displaySize));
-
-        validateSearchParameters(state, reference, email, cardBrand, fromDate, toDate, pageNumber, displaySize);
-
-        if (isNotBlank(cardBrand)) {
-            cardBrand = cardBrand.toLowerCase();
-        }
-
-        List<Pair<String, String>> queryParams = asList(
-                Pair.of("reference", reference),
-                Pair.of("email", email),
-                Pair.of("state", state),
-                Pair.of("card_brand", cardBrand),
-                Pair.of("from_date", fromDate),
-                Pair.of("to_date", toDate),
-                Pair.of("transactionType", "charge"),
-                Pair.of("page", pageNumber),
-                Pair.of("display_size", displaySize)
-        );
         
-        Response connectorResponse = client
-                .target(connectorUriGenerator.chargesURI(account, queryParams))
-                .request()
-                .header(HttpHeaders.ACCEPT, APPLICATION_JSON)
-                .get();
-
-        logger.info("response from connector form charge search: " + connectorResponse);
-
-        if (connectorResponse.getStatus() == SC_OK) {
-            try {
-                JsonNode responseJson = connectorResponse.readEntity(JsonNode.class);
-                logger.debug("json response from connector from charge search: " + responseJson);
-
-                TypeReference<PaymentSearchResponse> typeRef = new TypeReference<PaymentSearchResponse>() {};
-                PaymentSearchResponse searchResponse = objectMapper.readValue(responseJson.traverse(), typeRef);
-                List<PaymentForSearchResult> paymentsForSearchResults = searchResponse.getPayments()
-                        .stream()
-                        .map(charge -> PaymentForSearchResult.valueOf(
-                                charge,
-                                publicApiUriGenerator.getPaymentURI(charge.getChargeId()),
-                                publicApiUriGenerator.getPaymentEventsURI(charge.getChargeId()),
-                                publicApiUriGenerator.getPaymentCancelURI(charge.getChargeId()),
-                                publicApiUriGenerator.getPaymentRefundsURI(charge.getChargeId())))
-                        .collect(Collectors.toList());
-
-                HalRepresentation.HalRepresentationBuilder halRepresentation = HalRepresentation.builder()
-                        .addProperty("results", paymentsForSearchResults)
-                        .addProperty("count", searchResponse.getCount())
-                        .addProperty("total", searchResponse.getTotal())
-                        .addProperty("page", searchResponse.getPage());
-
-                addLink(halRepresentation, "self", transformIntoPublicUri(baseUrl, searchResponse.getLinks().getSelf()));
-                addLink(halRepresentation, "first_page", transformIntoPublicUri(baseUrl, searchResponse.getLinks().getFirstPage()));
-                addLink(halRepresentation, "last_page", transformIntoPublicUri(baseUrl, searchResponse.getLinks().getLastPage()));
-                addLink(halRepresentation, "prev_page", transformIntoPublicUri(baseUrl, searchResponse.getLinks().getPrevPage()));
-                addLink(halRepresentation, "next_page", transformIntoPublicUri(baseUrl, searchResponse.getLinks().getNextPage()));
-
-                return Response.ok(halRepresentation.build().toString()).build();
-            } catch (IOException | ProcessingException | URISyntaxException e) {
-                throw new SearchChargesException(e);
-            }
-        }
-        throw new SearchChargesException(connectorResponse);
+        HalRepresentation halRepresentation = paymentSearchService
+                .doSearch(account, 
+                        reference, 
+                        email,
+                        state,
+                        cardBrand,
+                        fromDate,
+                        toDate,
+                        pageNumber,
+                        displaySize,
+                        agreement);
+        return Response.ok(halRepresentation.toString()).build();
     }
-
-    private void addLink(HalRepresentation.HalRepresentationBuilder halRepresentationBuilder, String name, URI uri) {
-        if (uri != null) {
-            halRepresentationBuilder.addLink(name, uri);
-        }
-    }
-
-    private URI transformIntoPublicUri(String baseUrl, uk.gov.pay.api.model.links.Link link) throws URISyntaxException {
-        if (link == null)
-            return null;
-
-        return UriBuilder.fromUri(baseUrl)
-                .path("/v1/payments")
-                .replaceQuery(new URI(link.getHref()).getQuery())
-                .build();
-    }
-
+    
     @POST
     @Timed
     @Path("/v1/payments")
