fcb83e12d153e716695240ec564be71e16165f66
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2018-06-19 17:34:19 +0100

    PP-3676: Fix failing PR build
    PR build was failing on direct-debit-connector tests. This fix
    - uses a unique gateway account id as other tests have already used
      GATEWAY_ACCOUNT_ID as an id
    - relaxes the pattern matching for various links. we generally don't care about
      hostname and port as they are defined in the direct-debit-connector's
      configuration yaml which can be anything
    
    @oswaldquek

--- src/test/java/uk/gov/pay/api/service/CreateAgreementServiceTest.java
+++ src/test/java/uk/gov/pay/api/service/CreateAgreementServiceTest.java
@@ -52,7 +52,7 @@ public class CreateAgreementServiceTest {
     @PactVerification({"direct-debit-connector"})
     @Pacts(pacts = {"publicapi-direct-debit-connector-create-agreement"})
     public void shouldCreateAMandateSuccessfully() {
-        Account account = new Account("GATEWAY_ACCOUNT_ID", TokenPaymentType.DIRECT_DEBIT);
+        Account account = new Account("9ddfcc27-acf5-43f9-92d5-52247540714b", TokenPaymentType.DIRECT_DEBIT);
         CreateAgreementRequest requestPayload = new CreateAgreementRequest(
                 "https://example.com/return", AgreementType.ON_DEMAND);
         CreateAgreementResponse agreementResponse = createAgreementService.create(account, requestPayload);
@@ -62,7 +62,7 @@ public class CreateAgreementServiceTest {
         assertThat(agreementResponse.getCreatedDate(), is("2016-01-01T12:00:00.000Z"));
         assertThat(agreementResponse.getReturnUrl(), is("https://example.com/return"));
         assertThat(agreementResponse.getState(), is(AgreementStatus.CREATED));
-        assertThat(agreementResponse.getLinks().getSelf(), is(new Link("http://localhost:1234/v1/api/accounts/GATEWAY_ACCOUNT_ID/mandates/mandateId", "GET")));
+        assertThat(agreementResponse.getLinks().getSelf(), is(new Link("http://localhost:1234/v1/api/accounts/9ddfcc27-acf5-43f9-92d5-52247540714b/mandates/mandateId", "GET")));
         assertThat(agreementResponse.getLinks().getNextUrl(), is(new Link("http://frontend_direct_debit/secure/token_1234567asdf", "GET")));
         PostLink expectedLink = new PostLink("http://frontend_direct_debit/secure/", "POST", "application/x-www-form-urlencoded", Collections.singletonMap("chargeTokenId", "token_1234567asdf"));
         assertThat(agreementResponse.getLinks().getNextUrlPost(), is(expectedLink));
