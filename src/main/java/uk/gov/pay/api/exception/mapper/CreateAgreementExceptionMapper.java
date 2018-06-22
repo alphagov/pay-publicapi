ac93c66de7179739380e8ebaafce2aae29fafb0c
george racu <george.racu@digital.cabinet-office.gov.uk>
2018-06-22 08:33:55 +0100

    PP-3676 Replaced PaymentError with AgreementError
    with @danailminchev

--- src/main/java/uk/gov/pay/api/exception/mapper/CreateAgreementExceptionMapper.java
+++ src/main/java/uk/gov/pay/api/exception/mapper/CreateAgreementExceptionMapper.java
@@ -3,16 +3,14 @@ package uk.gov.pay.api.exception.mapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.gov.pay.api.exception.CreateAgreementException;
-import uk.gov.pay.api.model.PaymentError;
+import uk.gov.pay.api.model.directdebit.agreement.AgreementError;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 
 import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
 import static javax.ws.rs.core.Response.Status.NOT_FOUND;
-import static uk.gov.pay.api.model.PaymentError.Code.CREATE_PAYMENT_ACCOUNT_ERROR;
-import static uk.gov.pay.api.model.PaymentError.Code.CREATE_PAYMENT_CONNECTOR_ERROR;
-import static uk.gov.pay.api.model.PaymentError.aPaymentError;
+import static uk.gov.pay.api.model.directdebit.agreement.AgreementError.anAgreementError;
 
 public class CreateAgreementExceptionMapper implements ExceptionMapper<CreateAgreementException> {
 
@@ -20,14 +18,17 @@ public class CreateAgreementExceptionMapper implements ExceptionMapper<CreateAgr
 
     @Override
     public Response toResponse(CreateAgreementException exception) {
-        PaymentError paymentError = exception.getErrorStatus() == NOT_FOUND.getStatusCode() ?
-                aPaymentError(CREATE_PAYMENT_ACCOUNT_ERROR) : aPaymentError(CREATE_PAYMENT_CONNECTOR_ERROR);
 
-        LOGGER.error("Direct Debit connector invalid response was {}.\n Returning http status {} with error body {}", exception.getMessage(), INTERNAL_SERVER_ERROR, paymentError);
+        AgreementError agreementError = exception.getErrorStatus() == NOT_FOUND.getStatusCode()
+                ? anAgreementError(AgreementError.Code.CREATE_AGREEMENT_ACCOUNT_ERROR)
+                : anAgreementError(AgreementError.Code.CREATE_AGREEMENT_CONNECTOR_ERROR);
+
+        LOGGER.error("Direct Debit connector invalid response was {}.\n Returning http status {} with error body {}",
+                exception.getMessage(), INTERNAL_SERVER_ERROR, agreementError);
 
         return Response
                 .status(INTERNAL_SERVER_ERROR)
-                .entity(paymentError)
+                .entity(agreementError)
                 .build();
     }
 }
