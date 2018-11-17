680fd522e487ef0ac44c9621e558b6f847caf4fd
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2018-11-17 10:13:41 +0000

    BAU: Update usage of mockserver for 5.x
    mockserver has changed its package structure with the upgrade from 3.x to 5.x.
    Use the correct package for MockServerClient

--- src/test/java/uk/gov/pay/api/utils/mocks/ConnectorDDMockClient.java
+++ src/test/java/uk/gov/pay/api/utils/mocks/ConnectorDDMockClient.java
@@ -1,7 +1,7 @@
 package uk.gov.pay.api.utils.mocks;
 
 import com.google.common.collect.ImmutableMap;
-import org.mockserver.client.server.ForwardChainExpectation;
+import org.mockserver.client.ForwardChainExpectation;
 import org.mockserver.model.HttpResponse;
 import uk.gov.pay.api.model.PaymentState;
 import uk.gov.pay.api.model.directdebit.agreement.MandateState;
