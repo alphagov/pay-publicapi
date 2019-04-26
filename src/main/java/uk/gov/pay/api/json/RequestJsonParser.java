d8caa3eaeb9e22c76dc96405f35c4659b8cc963c
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2019-04-26 18:47:45 +0100

    PP-4886: Refactor RequestJsonParser
    Extract validations into its own method. This makes things clearer.

--- src/main/java/uk/gov/pay/api/json/RequestJsonParser.java
+++ src/main/java/uk/gov/pay/api/json/RequestJsonParser.java
@@ -2,11 +2,11 @@ package uk.gov.pay.api.json;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpStatus;
 import uk.gov.pay.api.exception.BadRequestException;
 import uk.gov.pay.api.model.CreatePaymentRefundRequest;
 import uk.gov.pay.api.model.CreatePaymentRequest;
 import uk.gov.pay.api.model.PaymentError;
+import uk.gov.pay.api.model.PaymentError.Code;
 import uk.gov.pay.api.validation.LanguageValidator;
 import uk.gov.pay.commons.model.charge.ExternalMetadata;
 
@@ -17,9 +17,10 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import static org.apache.http.HttpStatus.SC_UNPROCESSABLE_ENTITY;
 import static uk.gov.pay.api.model.CreatePaymentRefundRequest.REFUND_AMOUNT_AVAILABLE;
 import static uk.gov.pay.api.model.CreatePaymentRequest.AGREEMENT_ID_FIELD_NAME;
 import static uk.gov.pay.api.model.CreatePaymentRequest.AMOUNT_FIELD_NAME;
@@ -41,136 +42,121 @@ class RequestJsonParser {
     private static ObjectMapper objectMapper = new ObjectMapper();
     private static final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
-    static CreatePaymentRequest parsePaymentRequest(JsonNode paymentRequest) {
-        Integer amount = parseInteger(paymentRequest, AMOUNT_FIELD_NAME, true, CREATE_PAYMENT_VALIDATION_ERROR, CREATE_PAYMENT_MISSING_FIELD_ERROR);
-        String reference = parseString(paymentRequest, REFERENCE_FIELD_NAME, true);
-        String description = parseString(paymentRequest, DESCRIPTION_FIELD_NAME, true);
-
-        CreatePaymentRequest.CreatePaymentRequestBuilder createPaymentRequestBuilder = CreatePaymentRequest.builder()
-                .amount(amount)
-                .reference(reference)
-                .description(description);
-
-        if (paymentRequest.has(LANGUAGE_FIELD_NAME)) {
-            String language = parseString(paymentRequest, LANGUAGE_FIELD_NAME, false,
-                    aPaymentError(LANGUAGE_FIELD_NAME, CREATE_PAYMENT_VALIDATION_ERROR, LanguageValidator.ERROR_MESSAGE));
-            createPaymentRequestBuilder.language(language);
-        }
-
-        if (paymentRequest.has(DELAYED_CAPTURE_FIELD_NAME)) {
-            Boolean delayedCapture = parseBoolean(
-                    paymentRequest,
-                    DELAYED_CAPTURE_FIELD_NAME,
-                    false,
-                    CREATE_PAYMENT_VALIDATION_ERROR,
-                    CREATE_PAYMENT_MISSING_FIELD_ERROR
-            );
-            createPaymentRequestBuilder.delayedCapture(delayedCapture);
-        }
-
-        if (paymentRequest.has(AGREEMENT_ID_FIELD_NAME)) {
-            String agreementId = parseString(paymentRequest, AGREEMENT_ID_FIELD_NAME, true,
-                    aPaymentError(AGREEMENT_ID_FIELD_NAME, CREATE_PAYMENT_VALIDATION_ERROR, "Must be a valid agreement ID"));
-            createPaymentRequestBuilder.agreementId(agreementId);
-        } else {
-            String returnUrl = parseString(paymentRequest, RETURN_URL_FIELD_NAME, true,
-                    aPaymentError(RETURN_URL_FIELD_NAME, CREATE_PAYMENT_VALIDATION_ERROR, "Must be a valid URL format"));
-            createPaymentRequestBuilder.returnUrl(returnUrl);
-        }
-        
-        if (paymentRequest.has(METADATA)) {
-            Map<String, Object> metadataMap = null;
-            try {
-                metadataMap = objectMapper.convertValue(paymentRequest.get("metadata"), Map.class);
-            } catch (IllegalArgumentException e) {
-                PaymentError paymentError = new PaymentError(METADATA, CREATE_PAYMENT_METADATA_VALIDATION_ERROR, "Field [metadata] must be an object of JSON key-value pairs");
-                throw new WebApplicationException(Response.status(HttpStatus.SC_UNPROCESSABLE_ENTITY).entity(paymentError).build());
-            }
-            ExternalMetadata metadata = new ExternalMetadata(metadataMap);
-            Set<ConstraintViolation<ExternalMetadata>> violations = validator.validate(metadata);
-            if (violations.size() > 0) {
-                String message = violations.stream().map(v -> v.getMessage()).collect(Collectors.joining(";\n"));
-                PaymentError paymentError = new PaymentError(METADATA, CREATE_PAYMENT_METADATA_VALIDATION_ERROR, message);
-                throw new WebApplicationException(Response.status(HttpStatus.SC_UNPROCESSABLE_ENTITY).entity(paymentError).build());
-            }
-            createPaymentRequestBuilder.metadata(metadata);
-        }
-
-        return createPaymentRequestBuilder.build();
-    }
-
     static CreatePaymentRefundRequest parseRefundRequest(JsonNode rootNode) {
-        Integer amount = parseInteger(rootNode, AMOUNT_FIELD_NAME, true, CREATE_PAYMENT_REFUND_VALIDATION_ERROR, CREATE_PAYMENT_REFUND_MISSING_FIELD_ERROR);
+        Integer amount = validateAndGetAmount(rootNode, CREATE_PAYMENT_REFUND_VALIDATION_ERROR, CREATE_PAYMENT_REFUND_MISSING_FIELD_ERROR);
         Integer refundAmountAvailable = rootNode.get(REFUND_AMOUNT_AVAILABLE) == null ? null : rootNode.get(REFUND_AMOUNT_AVAILABLE).asInt();
         return new CreatePaymentRefundRequest(amount, refundAmountAvailable);
     }
+    
+    static CreatePaymentRequest parsePaymentRequest(JsonNode paymentRequest) {
 
-    private static String parseString(JsonNode node, String fieldName, boolean isFieldMandatory) {
-        return parseString(node, fieldName, isFieldMandatory, aPaymentError(fieldName, CREATE_PAYMENT_VALIDATION_ERROR,
-                "Must be a valid string format"));
+        var builder = CreatePaymentRequest.builder()
+                .amount(validateAndGetAmount(paymentRequest, CREATE_PAYMENT_VALIDATION_ERROR, CREATE_PAYMENT_MISSING_FIELD_ERROR))
+                .reference(validateAndGetReference(paymentRequest))
+                .description(validateAndGetDescription(paymentRequest));
+
+        if (paymentRequest.has(LANGUAGE_FIELD_NAME))
+            builder.language(validateAndGetLanguage(paymentRequest));
+
+        if (paymentRequest.has(DELAYED_CAPTURE_FIELD_NAME))
+            builder.delayedCapture(validateAndGetDelayedCapture(paymentRequest));
+
+        if (paymentRequest.has(AGREEMENT_ID_FIELD_NAME)) 
+            builder.agreementId(validateAndGetAgreementId(paymentRequest));
+        else builder.returnUrl(validateAndGetReturnUrl(paymentRequest));
+
+        if (paymentRequest.has(METADATA))
+            builder.metadata(validateAndGetMetadata(paymentRequest));
+
+        return builder.build();
     }
 
-    private static String parseString(JsonNode node, String fieldName, boolean isFieldMandatory, PaymentError formatError) {
-        JsonNode fieldNode = node.get(fieldName);
-        String fieldValue = getStringValue(fieldNode, formatError);
-        check(isNotBlank(fieldValue), isFieldMandatory ?
-                aPaymentError(fieldName, CREATE_PAYMENT_MISSING_FIELD_ERROR) :
-                formatError
-        );
-        return fieldValue;
+    private static String validateAndGetReturnUrl(JsonNode paymentRequest) {
+        return getStringValue(
+                paymentRequest.get(RETURN_URL_FIELD_NAME),
+                aPaymentError(RETURN_URL_FIELD_NAME, CREATE_PAYMENT_VALIDATION_ERROR, "Must be a valid URL format"),
+                aPaymentError(RETURN_URL_FIELD_NAME, CREATE_PAYMENT_MISSING_FIELD_ERROR));
     }
 
-    private static Integer parseInteger(JsonNode node, String fieldName, boolean isFieldMandatory, PaymentError.Code validationErrorCode, PaymentError.Code missingErrorCode) {
-        JsonNode fieldNode = node.get(fieldName);
-        PaymentError validationPaymentError = aPaymentError(fieldName, validationErrorCode, "Must be a valid numeric format");
-        Integer fieldValue = getIntegerValue(fieldNode, validationPaymentError);
-        check(fieldValue != null, isFieldMandatory ?
-                aPaymentError(fieldName, missingErrorCode) :
-                validationPaymentError
-        );
-        return fieldValue;
+    private static String validateAndGetAgreementId(JsonNode paymentRequest) {
+        return getStringValue(
+                paymentRequest.get(AGREEMENT_ID_FIELD_NAME),
+                aPaymentError(AGREEMENT_ID_FIELD_NAME, CREATE_PAYMENT_VALIDATION_ERROR, "Must be a valid agreement ID"),
+                aPaymentError(AGREEMENT_ID_FIELD_NAME, CREATE_PAYMENT_MISSING_FIELD_ERROR));
     }
 
-    private static Boolean parseBoolean(JsonNode node, String fieldName, boolean isFieldMandatory, PaymentError.Code validationErrorCode, PaymentError.Code missingErrorCode) {
-        JsonNode fieldNode = node.get(fieldName);
-        PaymentError validationPaymentError = aPaymentError(fieldName, validationErrorCode, "Must be true or false");
-        Boolean fieldValue = getBooleanValue(fieldNode, validationPaymentError);
-        check(fieldValue != null, isFieldMandatory ?
-                aPaymentError(fieldName, missingErrorCode) :
-                validationPaymentError
-        );
-        return fieldValue;
+    private static String validateAndGetLanguage(JsonNode paymentRequest) {
+        return getStringValue(
+                paymentRequest.get(LANGUAGE_FIELD_NAME),
+                aPaymentError(LANGUAGE_FIELD_NAME, CREATE_PAYMENT_VALIDATION_ERROR, LanguageValidator.ERROR_MESSAGE),
+                aPaymentError(LANGUAGE_FIELD_NAME, CREATE_PAYMENT_VALIDATION_ERROR, LanguageValidator.ERROR_MESSAGE));
     }
 
-    private static String getStringValue(JsonNode fieldNode, PaymentError formatError) {
-        String fieldValue = null;
-        if (notNull(fieldNode)) {
-            check(fieldNode.isTextual(), formatError);
-            fieldValue = fieldNode.asText();
+    private static String validateAndGetDescription(JsonNode paymentRequest) {
+        return getStringValue(
+                paymentRequest.get(DESCRIPTION_FIELD_NAME),
+                aPaymentError(DESCRIPTION_FIELD_NAME, CREATE_PAYMENT_VALIDATION_ERROR,"Must be a valid string format"),
+                aPaymentError(DESCRIPTION_FIELD_NAME, CREATE_PAYMENT_MISSING_FIELD_ERROR));
+    }
+
+    private static String validateAndGetReference(JsonNode paymentRequest) {
+        return getStringValue(
+                paymentRequest.get(REFERENCE_FIELD_NAME),
+                aPaymentError(REFERENCE_FIELD_NAME, CREATE_PAYMENT_VALIDATION_ERROR,"Must be a valid string format"),
+                aPaymentError(REFERENCE_FIELD_NAME, CREATE_PAYMENT_MISSING_FIELD_ERROR));
+    }
+    
+    private static String getStringValue(JsonNode jsonNode, PaymentError validationError, PaymentError missingError) {
+        return getValue(jsonNode, validationError, missingError, JsonNode::isTextual, JsonNode::asText);
+    }
+
+    private static Boolean validateAndGetDelayedCapture(JsonNode paymentRequest) {
+        return getValue(
+                paymentRequest.get(DELAYED_CAPTURE_FIELD_NAME),
+                aPaymentError(DELAYED_CAPTURE_FIELD_NAME, CREATE_PAYMENT_VALIDATION_ERROR, "Must be true or false"),
+                aPaymentError(DELAYED_CAPTURE_FIELD_NAME, CREATE_PAYMENT_VALIDATION_ERROR, "Must be true or false"),
+                JsonNode::isBoolean,
+                JsonNode::booleanValue);
+    }
+
+    private static Integer validateAndGetAmount(JsonNode paymentRequest, Code validationError, Code missingError) {
+        return getValue(
+                paymentRequest.get(AMOUNT_FIELD_NAME),
+                aPaymentError(AMOUNT_FIELD_NAME, validationError, "Must be a valid numeric format"),
+                aPaymentError(AMOUNT_FIELD_NAME, missingError),
+                JsonNode::isInt,
+                JsonNode::intValue);
+    }
+
+    private static ExternalMetadata validateAndGetMetadata(JsonNode paymentRequest) {
+        Map<String, Object> metadataMap = null;
+        try {
+            metadataMap = objectMapper.convertValue(paymentRequest.get("metadata"), Map.class);
+        } catch (IllegalArgumentException e) {
+            PaymentError paymentError = new PaymentError(METADATA, CREATE_PAYMENT_METADATA_VALIDATION_ERROR,
+                    "Field [metadata] must be an object of JSON key-value pairs");
+            throw new WebApplicationException(Response.status(SC_UNPROCESSABLE_ENTITY).entity(paymentError).build());
         }
-        return fieldValue;
-    }
-
-    private static Integer getIntegerValue(JsonNode fieldNode, PaymentError formatError) {
-        Integer fieldValue = null;
-        if (notNull(fieldNode)) {
-            check(fieldNode.isInt(), formatError);
-            fieldValue = fieldNode.intValue();
+        ExternalMetadata metadata = new ExternalMetadata(metadataMap);
+        Set<ConstraintViolation<ExternalMetadata>> violations = validator.validate(metadata);
+        if (violations.size() > 0) {
+            String message = violations.stream().map(v -> v.getMessage()).collect(Collectors.joining(";\n"));
+            PaymentError paymentError = new PaymentError(METADATA, CREATE_PAYMENT_METADATA_VALIDATION_ERROR, message);
+            throw new WebApplicationException(Response.status(SC_UNPROCESSABLE_ENTITY).entity(paymentError).build());
         }
-        return fieldValue;
+        return metadata;
     }
-
-    private static Boolean getBooleanValue(JsonNode fieldNode, PaymentError formatError) {
-        Boolean fieldValue = null;
-        if (notNull(fieldNode)) {
-            check(fieldNode.isBoolean(), formatError);
-            fieldValue = fieldNode.booleanValue();
+    
+    private static <T> T getValue(JsonNode jsonNode,
+                                  PaymentError validationError,
+                                  PaymentError missingError,
+                                  Function<JsonNode, Boolean> isExpectedType,
+                                  Function<JsonNode, T> valueFromJsonNode) {
+        if (jsonNode != null && !jsonNode.isNull()) {
+            check(isExpectedType.apply(jsonNode), validationError);
+            return valueFromJsonNode.apply(jsonNode);
         }
-        return fieldValue;
-    }
-
-    private static boolean notNull(JsonNode fieldNode) {
-        return fieldNode != null && !fieldNode.isNull();
+        throw new BadRequestException(missingError);
     }
 
     private static void check(boolean condition, PaymentError error) {
