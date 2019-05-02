1fc790ed28fdd83e2967900adbacd8497a634478
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-05-02 15:07:08 +0100

    PP-5147: Upgrade to Java 11.0.3
    The previous base image actually contained 11.0.2, so upgrade properly.
    
    Also, make use of the new -cacerts option to keytool, which means we no longer
    need to set JAVA_HOME (which is already set by adoptopenjdk anyway)
    
    This addresses the following CVEs:
    
    CVE-2019-2602
    CVE-2019-2699
    CVE-2019-2697
    CVE-2019-2698
    CVE-2019-2684
    
    The first one is potentially relevant as a DoS or potential crypto sidechannel,
    the others less so.

--- docker-startup.sh
+++ docker-startup.sh
@@ -4,12 +4,11 @@ set -eu
 
 if [ -n "${CERTS_PATH:-}" ]; then
   i=0
-  truststore=$JAVA_HOME/lib/security/cacerts
   truststore_pass=changeit
   for cert in "$CERTS_PATH"/*; do
     [ -f "$cert" ] || continue
-    echo "Adding $cert to $truststore"
-    keytool -importcert -noprompt -keystore "$truststore" -storepass "$truststore_pass" -file "$cert" -alias custom$((i++))
+    echo "Adding $cert to default truststore"
+    keytool -importcert -noprompt -cacerts -storepass "$truststore_pass" -file "$cert" -alias custom$((i++))
   done
 fi
 
