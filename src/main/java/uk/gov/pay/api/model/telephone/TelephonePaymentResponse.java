64359e5a590b7217a14caf96cf3083a238bde11c
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-07-30 16:20:10 +0100

    Removed @JsonProperty notation for TelephonePaymentResponse and assigned snake_case property to class instead

--- src/main/java/uk/gov/pay/api/model/telephone/TelephonePaymentResponse.java
+++ src/main/java/uk/gov/pay/api/model/telephone/TelephonePaymentResponse.java
@@ -1,61 +1,46 @@
 package uk.gov.pay.api.model.telephone;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class TelephonePaymentResponse {
-
-    @JsonProperty("amount")
+    
     private int amount;
-
-    @JsonProperty("reference")
+    
     private String reference;
-
-    @JsonProperty("description")
+    
     private String description;
-
-    @JsonProperty("created_date")
+    
     private String createdDate;
-
-    @JsonProperty("authorised_date")
+    
     private String authorisedDate;
-
-    @JsonProperty("processor_id")
+    
     private String processorId;
-
-    @JsonProperty("provider_id")
+    
     private String providerId;
-
-    @JsonProperty("auth_code")
+    
     private String authCode;
-
-    @JsonProperty("payment_outcome")
+    
     private PaymentOutcome paymentOutcome;
-
-    @JsonProperty("card_type")
+    
     private String cardType;
-
-    @JsonProperty("name_on_card")
+    
     private String nameOnCard;
-
-    @JsonProperty("email_address")
+    
     private String emailAddress;
-
-    @JsonProperty("card_expiry")
+    
     private String cardExpiry;
-
-    @JsonProperty("last_four_digits")
+    
     private String lastFourDigits;
-
-    @JsonProperty("first_six_digits")
+    
     private String firstSixDigits;
-
-    @JsonProperty("telephone_number")
+    
     private String telephoneNumber;
-
-    @JsonProperty("payment_id")
+    
     private String paymentId;
-
-    @JsonProperty("state")
+    
     private State state;
 
     public TelephonePaymentResponse() {
@@ -164,6 +149,8 @@ public class TelephonePaymentResponse {
     public String getTelephoneNumber() {
         return telephoneNumber;
     }
+
+    public String getPaymentId() { return paymentId; }
 
     public State getState() {
         return state;
