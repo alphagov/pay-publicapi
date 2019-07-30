0dcca67236cd99beaa8cbc4b2de598d5563cf2b2
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-07-30 11:22:49 +0100

    Refactors GetTelephonePaymentIT to check for valid JSON response as well as deleting JSON representation tests

--- src/main/java/uk/gov/pay/api/service/telephone/CreateTelephonePaymentService.java
+++ src/main/java/uk/gov/pay/api/service/telephone/CreateTelephonePaymentService.java
@@ -9,30 +9,12 @@ import uk.gov.pay.api.model.telephone.TelephonePaymentResponse;
 public class CreateTelephonePaymentService {
     
     public TelephonePaymentResponse create(CreateTelephonePaymentRequest createTelephonePaymentRequest){
-
-        boolean present = createTelephonePaymentRequest.getPaymentOutcome().getSupplemental().isPresent();
-
-
-        System.out.println("Present" + present);
-
-
+        
         Supplemental supplemental = createTelephonePaymentRequest.getPaymentOutcome().getSupplemental()
                 .map(s -> new Supplemental(
                         s.getErrorCode(),
                         s.getErrorMessage()
                 )).orElse(null);
-        
-        
-        //Supplemental supplemental;
-        
-//        if (createTelephonePaymentRequest.getPaymentOutcome().getSupplemental() == null) {
-//            supplemental = null;
-//        } else {
-//            supplemental = new Supplemental(
-//                    createTelephonePaymentRequest.getPaymentOutcome().getSupplemental().getErrorCode(),
-//                    createTelephonePaymentRequest.getPaymentOutcome().getSupplemental().getErrorMessage()
-//            );
-//        }
         
         PaymentOutcome paymentOutcome = new PaymentOutcome(
                 createTelephonePaymentRequest.getPaymentOutcome().getStatus(),
