b09702f05fee5b805bc044ec0d7685108b9b56a9
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-08-19 11:54:28 +0100

    PP-5443 Creates shouldGenerateTheRightTelephoneChargeURI test

--- src/main/java/uk/gov/pay/api/service/ConnectorUriGenerator.java
+++ src/main/java/uk/gov/pay/api/service/ConnectorUriGenerator.java
@@ -51,6 +51,10 @@ public class ConnectorUriGenerator {
         String path = format("/v1/api/accounts/%s/charges/%s/cancel", account.getAccountId(), paymentId);
         return buildConnectorUri(path, Maps.newHashMap());
     }
+    
+    String telephoneChargesURI(Account account) {
+        return null;
+    }
 
     public String eventsURI(Account account, Optional<ZonedDateTime> toDate, Optional<ZonedDateTime> fromDate, Integer page, Integer displaySize, String agreementId, String paymentId) {
 
