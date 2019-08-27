6078b2209b058094305100191c1bb9dacc3eb591
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-08-27 10:58:27 +0100

    PP-5443 Adds further validation tests to CardExpiryValidationIT

--- src/test/java/uk/gov/pay/api/it/telephone/CardExpiryValidationIT.java
+++ src/test/java/uk/gov/pay/api/it/telephone/CardExpiryValidationIT.java
@@ -44,4 +44,56 @@ public class CardExpiryValidationIT extends TelephonePaymentResourceITBase {
         postPaymentResponse(payload)
                 .statusCode(422);
     }
+
+    @Test
+    public void respondWith422_whenMonthis99() {
+        HashMap<String, Object> request_body = new HashMap<>();
+        request_body.put("amount", 100);
+        request_body.put("reference", "Some reference");
+        request_body.put("description", "Some description");
+        request_body.put("created_date", "2018-02-21T16:04:25Z");
+        request_body.put("authorised_date", "2018-02-21T16:05:33Z");
+        request_body.put("processor_id", "1PROC");
+        request_body.put("provider_id", "1PROV");
+        request_body.put("auth_code", "666");
+        request_body.put("payment_outcome", Map.of("status", "success"));
+        request_body.put("card_type", "visa");
+        request_body.put("name_on_card", "Jane Doe");
+        request_body.put("email_address", "jane_doe@example.com");
+        request_body.put("card_expiry", "99/99");
+        request_body.put("last_four_digits", "1234");
+        request_body.put("first_six_digits", "123456");
+        request_body.put("telephone_number", "+447700900796");
+
+        String payload = toJson(request_body);
+
+        postPaymentResponse(payload)
+                .statusCode(422);
+    }
+
+    @Test
+    public void respondWith422_whenMonthisNull() {
+        HashMap<String, Object> request_body = new HashMap<>();
+        request_body.put("amount", 100);
+        request_body.put("reference", "Some reference");
+        request_body.put("description", "Some description");
+        request_body.put("created_date", "2018-02-21T16:04:25Z");
+        request_body.put("authorised_date", "2018-02-21T16:05:33Z");
+        request_body.put("processor_id", "1PROC");
+        request_body.put("provider_id", "1PROV");
+        request_body.put("auth_code", "666");
+        request_body.put("payment_outcome", Map.of("status", "success"));
+        request_body.put("card_type", "visa");
+        request_body.put("name_on_card", "Jane Doe");
+        request_body.put("email_address", "jane_doe@example.com");
+        request_body.put("card_expiry", null);
+        request_body.put("last_four_digits", "1234");
+        request_body.put("first_six_digits", "123456");
+        request_body.put("telephone_number", "+447700900796");
+
+        String payload = toJson(request_body);
+
+        postPaymentResponse(payload)
+                .statusCode(422);
+    }
 }
