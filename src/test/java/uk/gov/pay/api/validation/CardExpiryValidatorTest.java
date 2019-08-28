d9bb4a3a9dbeaca89b69623a7bc2b36f94518d0a
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-08-28 20:06:09 +0100

    PP-5443 Creates fixture for building a CreateTelephonePaymentRequest and creates a failsValidationForInvalidCardType() test

--- src/test/java/uk/gov/pay/api/validation/CardExpiryValidatorTest.java
+++ src/test/java/uk/gov/pay/api/validation/CardExpiryValidatorTest.java
@@ -1,11 +1,18 @@
 package uk.gov.pay.api.validation;
 
 import org.junit.BeforeClass;
+import org.junit.Test;
 import uk.gov.pay.api.model.telephone.CreateTelephonePaymentRequest;
+import uk.gov.pay.api.model.telephone.PaymentOutcome;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class CardExpiryValidatorTest {
 
@@ -17,5 +24,35 @@ public class CardExpiryValidatorTest {
     public static void setUpValidator() {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
+
+        telephoneRequestBuilder
+                .amount(1200)
+                .description("Some description")
+                .reference("Some reference")
+                .createdDate("2018-02-21T16:04:25Z")
+                .authorisedDate("2018-02-21T16:05:33Z")
+                .authCode("666")
+                .processorId("1PROC")
+                .providerId("1PROV")
+                .lastFourDigits("1234")
+                .firstSixDigits("123456")
+                .cardType("visa")
+                .nameOnCard("Jane Doe")
+                .emailAddress("jane_doe@example.com")
+                .telephoneNumber("+447700900796")
+                .paymentOutcome(new PaymentOutcome("success"));
+    }
+
+    @Test
+    public void failsValidationForInvalidCardType() {
+
+        CreateTelephonePaymentRequest telephonePaymentRequest = telephoneRequestBuilder
+                .cardExpiry("99/99")
+                .build();
+
+        Set<ConstraintViolation<CreateTelephonePaymentRequest>> constraintViolations = validator.validate(telephonePaymentRequest);
+
+        assertThat(constraintViolations.size(), is(1));
+        assertThat(constraintViolations.iterator().next().getMessage(), is("Field [card_expiry] must have valid MM/YY"));
     }
 }
