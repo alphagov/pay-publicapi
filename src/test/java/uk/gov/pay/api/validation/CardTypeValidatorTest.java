148db35d16c9ea1cc92733aea93d8af499968e88
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-08-29 13:14:50 +0100

    PP-5443 Adds failsValidationForInvalidCardType test in CardTypeValidatorTest

--- src/test/java/uk/gov/pay/api/validation/CardTypeValidatorTest.java
+++ src/test/java/uk/gov/pay/api/validation/CardTypeValidatorTest.java
@@ -1,5 +1,61 @@
 package uk.gov.pay.api.validation;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+import uk.gov.pay.api.model.telephone.CreateTelephonePaymentRequest;
+import uk.gov.pay.api.model.telephone.PaymentOutcome;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class CardTypeValidatorTest {
+
+    private static CreateTelephonePaymentRequest.TelephoneRequestBuilder telephoneRequestBuilder = new CreateTelephonePaymentRequest.TelephoneRequestBuilder();
+
+    private static Validator validator;
+
+    @BeforeClass
+    public static void setUpValidator() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+
+        telephoneRequestBuilder
+                .amount(1200)
+                .description("Some description")
+                .reference("Some reference")
+                .createdDate("2018-02-21T16:04:25Z")
+                .authorisedDate("2018-02-21T16:05:33Z")
+                .authCode("666")
+                .processorId("1PROC")
+                .providerId("1PROV")
+                .cardExpiry("01/99")
+                .lastFourDigits("1234")
+                .firstSixDigits("123456")
+                .nameOnCard("Jane Doe")
+                .emailAddress("jane_doe@example.com")
+                .telephoneNumber("+447700900796")
+                .paymentOutcome(new PaymentOutcome("success"));
+    }
+
+    @Test
+    public void failsValidationForInvalidCardType() {
+
+        CreateTelephonePaymentRequest telephonePaymentRequest = telephoneRequestBuilder
+                .cardType("bad-card")
+                .build();
+
+        Set<ConstraintViolation<CreateTelephonePaymentRequest>> constraintViolations = validator.validate(telephonePaymentRequest);
+
+        assertThat(constraintViolations.size(), is(1));
+        assertThat(constraintViolations.iterator().next().getMessage(), is("Field [card_type] must be either master-card, visa, maestro, diners-club or american-express"));
+    }
+    
+    
 
 }
