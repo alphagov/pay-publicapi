7a04d67d13f0b53f96b6dde9ae1c36c33e96c570
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-08-29 16:06:59 +0100

    PP-5443 Refactors FirstSixCardDigitsValidatorIT to use a single HashMap as a test fixture in FirstSixCardDigitsValidationIT

--- src/test/java/uk/gov/pay/api/it/telephone/FirstSixCardDigitsValidationIT.java
+++ src/test/java/uk/gov/pay/api/it/telephone/FirstSixCardDigitsValidationIT.java
@@ -25,8 +25,9 @@ public class FirstSixCardDigitsValidationIT extends TelephonePaymentResourceITBa
         requestBody.put("provider_id", "1PROV");
         requestBody.put("payment_outcome", Map.of("status", "success"));
         requestBody.put("card_type", "visa");
+        requestBody.put("card_expiry", "00/99");
         requestBody.put("last_four_digits", "1234");
-        requestBody.put("first_six_digits", "123456");
+        
     }
 
     @After
@@ -36,79 +37,22 @@ public class FirstSixCardDigitsValidationIT extends TelephonePaymentResourceITBa
     
     @Test
     public void respondWith422_whenFiveDigitsProvideOnly() {
-        HashMap<String, Object> request_body = new HashMap<>();
-        request_body.put("amount", 100);
-        request_body.put("reference", "Some reference");
-        request_body.put("description", "Some description");
-        request_body.put("created_date", "2018-02-21T16:04:25Z");
-        request_body.put("authorised_date", "2018-02-21T16:05:33Z");
-        request_body.put("processor_id", "1PROC");
-        request_body.put("provider_id", "1PROV");
-        request_body.put("auth_code", "666");
-        request_body.put("payment_outcome", Map.of("status", "success"));
-        request_body.put("card_type", "visa");
-        request_body.put("name_on_card", "Jane Doe");
-        request_body.put("email_address", "jane_doe@example.com");
-        request_body.put("card_expiry", "01/99");
-        request_body.put("last_four_digits", "1234");
-        request_body.put("first_six_digits", "12345");
-        request_body.put("telephone_number", "+447700900796");
-
-        String payload = toJson(request_body);
-        
-        postPaymentResponse(payload)
+        requestBody.put("first_six_digits", "12345");
+        postPaymentResponse(toJson(requestBody))
                 .statusCode(422);
     }
 
     @Test
     public void respondWith422_whenSevenDigitsProvideOnly() {
-        HashMap<String, Object> request_body = new HashMap<>();
-        request_body.put("amount", 100);
-        request_body.put("reference", "Some reference");
-        request_body.put("description", "Some description");
-        request_body.put("created_date", "2018-02-21T16:04:25Z");
-        request_body.put("authorised_date", "2018-02-21T16:05:33Z");
-        request_body.put("processor_id", "1PROC");
-        request_body.put("provider_id", "1PROV");
-        request_body.put("auth_code", "666");
-        request_body.put("payment_outcome", Map.of("status", "success"));
-        request_body.put("card_type", "visa");
-        request_body.put("name_on_card", "Jane Doe");
-        request_body.put("email_address", "jane_doe@example.com");
-        request_body.put("card_expiry", "01/99");
-        request_body.put("last_four_digits", "1234");
-        request_body.put("first_six_digits", "1234567");
-        request_body.put("telephone_number", "+447700900796");
-
-        String payload = toJson(request_body);
-
-        postPaymentResponse(payload)
+        requestBody.put("first_six_digits", "1234567");
+        postPaymentResponse(toJson(requestBody))
                 .statusCode(422);
     }
 
     @Test
     public void respondWith422_whenFirstSixDigitsIsNull() {
-        HashMap<String, Object> request_body = new HashMap<>();
-        request_body.put("amount", 100);
-        request_body.put("reference", "Some reference");
-        request_body.put("description", "Some description");
-        request_body.put("created_date", "2018-02-21T16:04:25Z");
-        request_body.put("authorised_date", "2018-02-21T16:05:33Z");
-        request_body.put("processor_id", "1PROC");
-        request_body.put("provider_id", "1PROV");
-        request_body.put("auth_code", "666");
-        request_body.put("payment_outcome", Map.of("status", "success"));
-        request_body.put("card_type", "visa");
-        request_body.put("name_on_card", "Jane Doe");
-        request_body.put("email_address", "jane_doe@example.com");
-        request_body.put("card_expiry", "01/08");
-        request_body.put("last_four_digits", "1234");
-        request_body.put("first_six_digits", null);
-        request_body.put("telephone_number", "+447700900796");
-
-        String payload = toJson(request_body);
-
-        postPaymentResponse(payload)
+        requestBody.put("first_six_digits", null);
+        postPaymentResponse(toJson(requestBody))
                 .statusCode(422);
     }
 }
