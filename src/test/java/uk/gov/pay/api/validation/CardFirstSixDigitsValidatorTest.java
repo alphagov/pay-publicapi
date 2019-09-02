6bac1f407bcc35015c069d4e79cee79f75a771d2
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-09-02 10:09:09 +0100

    PP-5443 Uses required data for CardFirstSixDigitsValidatorTest

--- src/test/java/uk/gov/pay/api/validation/CardFirstSixDigitsValidatorTest.java
+++ src/test/java/uk/gov/pay/api/validation/CardFirstSixDigitsValidatorTest.java
@@ -29,17 +29,11 @@ public class CardFirstSixDigitsValidatorTest {
                 .amount(1200)
                 .description("Some description")
                 .reference("Some reference")
-                .createdDate("2018-02-21T16:04:25Z")
-                .authorisedDate("2018-02-21T16:05:33Z")
-                .authCode("666")
                 .processorId("1PROC")
                 .providerId("1PROV")
                 .cardExpiry("01/99")
                 .cardType("visa")
                 .lastFourDigits("1234")
-                .nameOnCard("Jane Doe")
-                .emailAddress("jane_doe@example.com")
-                .telephoneNumber("+447700900796")
                 .paymentOutcome(new PaymentOutcome("success"));
     }
     
