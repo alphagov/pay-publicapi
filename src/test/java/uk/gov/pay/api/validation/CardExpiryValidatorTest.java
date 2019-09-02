56b4b05f70f8c3b5a9b60691c67f8b3a0fe17ad0
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-09-02 10:08:07 +0100

    PP-5443 Uses only required fields in CardExpiryValidatorTest

--- src/test/java/uk/gov/pay/api/validation/CardExpiryValidatorTest.java
+++ src/test/java/uk/gov/pay/api/validation/CardExpiryValidatorTest.java
@@ -29,17 +29,11 @@ public class CardExpiryValidatorTest {
                 .amount(1200)
                 .description("Some description")
                 .reference("Some reference")
-                .createdDate("2018-02-21T16:04:25Z")
-                .authorisedDate("2018-02-21T16:05:33Z")
-                .authCode("666")
                 .processorId("1PROC")
                 .providerId("1PROV")
                 .lastFourDigits("1234")
                 .firstSixDigits("123456")
                 .cardType("visa")
-                .nameOnCard("Jane Doe")
-                .emailAddress("jane_doe@example.com")
-                .telephoneNumber("+447700900796")
                 .paymentOutcome(new PaymentOutcome("success"));
     }
 
