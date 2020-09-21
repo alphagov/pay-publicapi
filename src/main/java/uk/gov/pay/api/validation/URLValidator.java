5567a911a73242b32f19f8e79fdb0e5b14c5735f
David Heath <david.heath@digital.cabinet-office.gov.uk>
2020-09-21 19:01:33 +0100

    Change URLValidator implementation to avoid initialization exception
    Was getting
    
    ```
    java.lang.ExceptionInInitializerError
    	at uk.gov.pay.api.validation.URLValidatorTest.whenUrlIsBlank_shouldFailValidation_whenEnabledSecureConnection(URLValidatorTest.java:98)
    Caused by: java.lang.IllegalStateException: Can only invoke this method before calling getInstance
    	at uk.gov.pay.api.validation.URLValidatorTest.whenUrlIsBlank_shouldFailValidation_whenEnabledSecureConnection(URLValidatorTest.java:98)
    ```
    
    something has changed in the update of commons-validator from 1.6 to 1.7 which likely initializes `UrlValidator` and therefore causes the static initializer segment to not run on time.
    
    The order of execution of static blocks in enums is non-intuitve. In fact the enum instances `SECURITY_ENABLED` and `SECURITY_DISABLED` are initialzed first before the static initializer runs.
    
    Refactor the implementation to avoid using `DomainValidator.updateTLDOverride` (which operats on the global/static level) and instead to pass in a `DomainValidator` instance with the appropriate overrides applied when the `UrlValidator` instance is created.
    
    This seems to make the tests pass

--- src/main/java/uk/gov/pay/api/validation/URLValidator.java
+++ src/main/java/uk/gov/pay/api/validation/URLValidator.java
@@ -4,36 +4,43 @@ import org.apache.commons.validator.routines.DomainValidator;
 import org.apache.commons.validator.routines.DomainValidator.ArrayType;
 import org.apache.commons.validator.routines.UrlValidator;
 
+import java.util.List;
+
 import static org.apache.commons.validator.routines.UrlValidator.ALLOW_2_SLASHES;
 import static org.apache.commons.validator.routines.UrlValidator.ALLOW_LOCAL_URLS;
 
-public enum URLValidator {
-    SECURITY_ENABLED {
-        private final UrlValidator URL_VALIDATOR = new UrlValidator(new String[]{"https"}, ALLOW_LOCAL_URLS + ALLOW_2_SLASHES);
+public class URLValidator {
+    private UrlValidator urlValidator;
+    
+    private URLValidator(boolean allowInsecureConnection) {
+        long options = ALLOW_LOCAL_URLS + ALLOW_2_SLASHES;
+        urlValidator = new UrlValidator(
+                schemes(allowInsecureConnection),
+                null, 
+                options, 
+                domainValidator()
+        );
+    }
 
-        @Override
-        public boolean isValid(String value) {
-            return URL_VALIDATOR.isValid(value);
+    private String[] schemes(boolean allowInsecureConnection) {
+        if (allowInsecureConnection) {
+            return new String[]{"http", "https"};
+        } else {
+            return new String[]{"https"};
         }
+    }
 
-    }, 
-    SECURITY_DISABLED {
-        private final UrlValidator URL_VALIDATOR = new UrlValidator(new String[]{"http", "https"}, ALLOW_LOCAL_URLS + ALLOW_2_SLASHES);
-
-        @Override
-        public boolean isValid(String value) {
-            return URL_VALIDATOR.isValid(value);
-        }
-    };
-
-    static {
+    private DomainValidator domainValidator() {
         String[] otherValidTlds = new String[]{"internal", "local"};
-        DomainValidator.updateTLDOverride(ArrayType.GENERIC_PLUS, otherValidTlds);
+        DomainValidator.Item item = new DomainValidator.Item(ArrayType.GENERIC_PLUS, otherValidTlds);
+        return DomainValidator.getInstance(true, List.of(item));
     }
 
-    public static URLValidator urlValidatorValueOf(boolean isDisabledSecureConnection) {
-        return isDisabledSecureConnection ? SECURITY_DISABLED : SECURITY_ENABLED;
+    public static URLValidator urlValidatorValueOf(boolean allowInsecureConnection) {
+        return new URLValidator(allowInsecureConnection);
     }
 
-    public abstract boolean isValid(String value);
+    public boolean isValid(String value) {
+        return urlValidator.isValid(value);
+    }
 }
