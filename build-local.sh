5b42ec696a42fcb6b04d3e7cc54724917a48d0b5
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2023-09-19 11:47:47 +0100

    PP-11154: Update initialise prometheus metrics

--- build-local.sh
+++ build-local.sh
@@ -5,4 +5,8 @@ set -e
 cd "$(dirname "$0")"
 
 mvn -DskipITs clean verify
-docker build -t govukpay/publicapi:local .
+if [ "$(uname -m)" == "arm64" ]; then
+  docker build -t governmentdigitalservice/pay-publicapi:local -f m1/arm64.Dockerfile .
+else
+  docker build -t governmentdigitalservice/pay-publicapi:local .
+fi
